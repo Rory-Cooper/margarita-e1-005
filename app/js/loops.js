099a42dfdfa5e537a9584d46c2d85a3127d83554
Rory Cooper <rorycooper911@gmail.com>
2018-05-19 15:51:36 +0300

    Добавила файл с практическим решением задач по js.

--- app/js/loops.js
+++ app/js/loops.js
@@ -11,6 +11,35 @@ while (i >= 0) {
 //2
 isAdmin = true ? alert('Привет, Admin!'): alert('Ты точно не Admin');
 
+//3 C помощью функции получилось бы вкуснее)
+var login = prompt('Введите логин');
+
+if (login === 'Admin') {
+  var password = prompt('Введите пароль');
+  if (password === 'MeGaHaTsKeR_666'){
+    alert (`Добро пожаловать, ${login}!`);
+  } else if (password === null) {
+    alert (`Введите пароль.`);
+  } else {
+    alert(`Пароль введен не верно.`);
+}
+
+
+} else if (login === 'User') {
+  var password = prompt('Введите пароль');
+  if (password === 'qwerty1234'){
+    alert (`Добро пожаловать, ${login}!`);
+  } else if (password === null) {
+    alert (`Введите пароль.`);
+  } else {
+    alert(`Пароль введен не верно.`);
+} 
+
+}else {
+    alert('Такого логина нет в системе.');
+}
+
+
 //4
 const age = +prompt('Сколько тебе лет?', '12');
 
