dda54086e15307346132c13c0f70b1c0a1b711df
Rory Cooper <rorycooper911@gmail.com>
2018-06-30 18:10:33 +0300

    Выполнила последнюю дз

--- app/js/R2D2.js
+++ app/js/R2D2.js
@@ -1,74 +1,73 @@
 const R2D2 = {
-	pow: function(x,n) {
-  		if (isNaN(x) || isNaN(n)) {
-        console.log('Введите верное значение');
-        } else if (n != 0) {
-		function func(x, n) {
+  pow(x, n) {
+    if (isNaN(x) || isNaN(n)) {
+      console.log('Введите верное значение');
+    } else if (n !== 0) {
+      function func(x, n) {
         if (n === 0) {
-        return 1;
+          return 1;
         }
         return x * func(x, n - 1);
-        }
-		const her = func(x, n);
-		console.log(her);
-		} else {
-        console.log(`1`);
-        }
-        },
-	max: function (a,b) {
-		let c = Number(a)-b;
-		if (isNaN(a)||isNaN(b)){
-		console.log (`Введите верное значение`);
-		} else if (c>0) {
-		return a;
-		}else if (c<0) {
-		return b;
-		} else {
-		console.log (`Значение равны между собой`);
-		}
-		},
-	min: function (a,b) {
-		let c = Number(a)-b;
-		if (isNaN(a)||isNaN(b)){
-		console.log (`Введите верное значение`);
-		} else if (c<0) {
-		return a;
-		} else if (c>0) {
-		return b;
-		} else {
-		console.log (`Значение равны между собой`);
-		}
-		},
-	add: function (a,b) {
-		if (isNaN(a)||isNaN(b)){
-		console.log (`Введите верное значение`);
-		} else {
-  		return Number(a)+Number(b); 
-		}
-		},
-	diam: function (len) {
-		if (len>0 ) {
-		return (len/3.141592).toFixed(0);
-		} else {
-		console.log (`Введите верное значение`);
-		}
-		},
-	disk: function (a,b,c) {
-		if (isNaN(a)||isNaN(b)||isNaN(c)){
-		console.log (`Введите верное значение`);
-		} else {
-		return b*b-4*c*a;
-		}
-		}};
+      }
+      const her = func(x, n);
+      console.log(her);
+    } else {
+      console.log('1');
+    }
+  },
+  max(a, b) {
+    const c = Number(a) - b;
+    if (isNaN(a) || isNaN(b)) {
+      console.log('Введите верное значение');
+    } else if (c > 0) {
+      return a;
+    } else if (c < 0) {
+      return b;
+    } else {
+      console.log('Значение равны между собой');
+    }
+  },
+  min(a, b) {
+    const c = Number(a) - b;
+    if (isNaN(a) || isNaN(b)) {
+      console.log('Введите верное значение');
+    } else if (c < 0) {
+      return a;
+    } else if (c > 0) {
+      return b;
+    } else {
+      console.log('Значение равны между собой');
+    }
+  },
+  add(a, b) {
+    if (isNaN(a) || isNaN(b)) {
+      console.log('Введите верное значение');
+    } else {
+      return Number(a) + Number(b);
+    }
+  },
+  diam(len) {
+    if (len > 0) {
+      return (len / 3.141592).toFixed(0);
+    }
+    console.log('Введите верное значение');
+  },
+  disk(a, b, c) {
+    if (isNaN(a) || isNaN(b) || isNaN(c)) {
+      console.log('Введите верное значение');
+    } else {
+      return b * b - 4 * c * a;
+    }
+  } };
 
-Object.defineProperty(R2D2, "E", {
+Object.defineProperty(R2D2, 'E', {
   value: 2.718281828459045,
-  writable: false
+  writable: false,
 });
 
-Object.defineProperty(R2D2, "PI", {
+Object.defineProperty(R2D2, 'PI', {
   value: 3.14159265359,
-  writable: false
+  writable: false,
 });
 
-R2D2.pow(2,2);
+R2D2.pow(2, 2);
--- app/js/function.js
+++ app/js/function.js
@@ -1,75 +1,68 @@
-const add = (a,b) => {
-  console.log (`1)`,Number(a)+Number(b)); 
-}
+const add = (a, b) => {
+  console.log('1)', Number(a) + Number(b));
+};
 add(20, 50);
-add(100,"2");
-
+add(100, '2');
 
 
 const length = (r) => {
-  console.log (`2)`,2*Math.PI*r);
-}
+  console.log('2)', 2 * Math.PI * r);
+};
 length(10);
 
 
-
 const discr = (a, b, c) => {
-  console.log (`3)`,b*b-4*c*a);
-}
-discr(2,5,-7);
-
+  console.log('3)', b * b - 4 * c * a);
+};
+discr(2, 5, -7);
 
 
 const square = (num) => {
-  console.log (`4)`,num*num);
-}
+  console.log('4)', num * num);
+};
 square(2);
 square(10);
 
 
-const sumTo=(n)=> {
-  if (n!=1) {
-    return Number(n)+sumTo(n-1);
-  } else {
-    return 1;
-}
-}
+const sumTo = (n) => {
+  if (n !== 1) {
+    return Number(n) + sumTo(n - 1);
+  }
+  return 1;
+};
 
-console.log(`5)`, sumTo(10) );
-console.log(`5)`, sumTo(100) );
+console.log('5)', sumTo(10));
+console.log('5)', sumTo(100));
 
 
 const fib = (num) => {
   if (num < 2) {
     return num;
-  } else {
-    return (fib(num - 1) + fib(num - 2));
   }
-}
-console.log(`6)`, fib(6) );
-console.log(`6)`, fib(11) );
+  return (fib(num - 1) + fib(num - 2));
+};
+console.log('6)', fib(6));
+console.log('6)', fib(11));
 
 
-const pow=(x,n)=> {
-  if (n!=0) {
-    return x*pow(x,n-1);
-  } else {
-    return 1;
-}
-}
+const pow = (x, n) => {
+  if (n !== 0) {
+    return x * pow(x, n - 1);
+  }
+  return 1;
+};
 
-console.log(`7)`, pow(2,4) );
-console.log(`7)`, pow(10,3) );
+console.log('7)', pow(2, 4));
+console.log('7)', pow(10, 3));
 
 
-const fact=(num)=> {
-  if (num!=1) {
-    return num*fact(num-1);
-  } else {
-    return 1;
-}
-}
+const fact = (num) => {
+  if (num !== 1) {
+    return num * fact(num - 1);
+  }
+  return 1;
+};
 
-console.log(`8)`, fact(5) );
-console.log(`8)`, fact(10) );
+console.log('8)', fact(5));
+console.log('8)', fact(10));
 
